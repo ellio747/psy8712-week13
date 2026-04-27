c47afffa7f2b2e0d3437abce7ba50a31f0d961f7
Ben Elliott <ellio747@umn.edu>
2026-04-27 07:48:19 -0500

    final commit updated scripting with final comments

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -1,14 +1,14 @@
-#Script Settings and Resources 
+# Script Settings and Resources 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) 
 library(DBI)
 library(RPostgres)
 library(tidyverse)
 
-#Data Import and Cleaning 
+# Data Import and Cleaning 
 con <- DBI::dbConnect( # Following instructions for connecting to SQL database in lesson slides
   RPostgres::Postgres(),
-  user = Sys.getenv("NEON_USER"), # maintained a User name and password in the system files on my local harddrive
-  password = Sys.getenv("NEON_PW"), # maintained a User name and password in the system files on my local harddrive
+  user = Sys.getenv("NEON_USER"), # maintained a User name and password in the .Renviron file on my local harddrive usethis::edit_r_environ()
+  password = Sys.getenv("NEON_PW"), # maintained a User name and password in the .Renviron file on my local harddrive usethis::edit_r_environ()
   host = "ep-billowing-union-am14lcnh-pooler.c-5.us-east-1.aws.neon.tech", #host location from instructions
   port = 5432, #port
   dbname = "neondb", #databse name
@@ -29,7 +29,7 @@ offices_tbl <- dbGetQuery(con, "SELECT * FROM datascience_offices") %>%
 
 # Combine data such that employees without test scores are removed
 week13_tbl <- testscores_tbl %>% # this retains an individual with a test score = 0, but not NULL, so retained
-  left_join( #left join starts with those that have test scores, but adds those managers without
+  left_join( # anchors on testscores_tbl, pulling in employee details; 22 employees without test scores are dropped
     employees_tbl, # employee data
     by = "employee_id" # linking key of employee_id in both tables
   ) %>% 
@@ -39,17 +39,19 @@ week13_tbl <- testscores_tbl %>% # this retains an individual with a test score 
   ) %>% 
   write_csv("../out/week13.csv") # writes to the out folder a csv of the week13_tbl
 
-#Analysis
+# Analysis
 # Display the total number of managers
 week13_tbl %>% 
+  filter(manager_hire == "Y") %>% # when asked Richard, "hire flag indicates they are currently a manager"
   count() %>% # this counts all managers
-  View()# n = 549 managers
+  View()# n = 28 managers
 
-# Display the total number of unique managers
+# Display the total number of unique managers; all subjects are unique managers
 week13_tbl %>% 
+  filter(manager_hire == "Y") %>% # when asked Richard, "hire flag indicates they are currently a manager"
   distinct(employee_id) %>% #this counts the number of unique managers based on employee_id
-  count()%>% # adds counting function
-  View()# n = 549 managers
+  count() %>% # adds counting function
+  View()# n = 28 distinct managers
 
 # Display summary of the number of managers split by location (only not original manager hires)
 week13_tbl %>% 
@@ -59,16 +61,19 @@ week13_tbl %>%
   View() # Chicago = 61, Houston = 20, New York = 183, Orlando = 20, San Francisco = 48, Toronto = 189
 
 # Display mean, SD of years of employment, split by performance level
-week13_tbl %>% 
+week13_tbl %>% # for this call, I grouped these by performance, without including manager flag, due to unspecificity in the question in the prompt
+  # filter(manager_hire == "Y") %>% # could include this call if looking specifically for managers
+  mutate(performance_group = factor(performance_group, levels = c("Bottom", "Middle", "Top"))) %>%
   group_by(performance_group) %>% # groups by performance level
   summarise(
-    m = mean(yrs_employed), # summarize means
-    sd = sd(yrs_employed) # summarize sds
+    m = round(mean(yrs_employed), 2), # summarize means; rounded 2 decimal for cleaner output
+    sd = round(sd(yrs_employed), 2) # summarize sds; rounded 2 decimal for cleaner output
   )%>% 
   View() # Bottom = 4.74 (.537), Middle = 4.58 (.509), Top = 4.33 (.604)
 
 # Display location classification, ID number, test score, in alphabetical order by location type, test score descending
 week13_tbl %>% 
+  filter(manager_hire == "Y") %>% # for this call, I filtered by manager flag as the prompt stated "manager's location"
   select(office_type, employee_id, test_score) %>% # retains location classification, ID number, and test score
   arrange(office_type, desc(test_score)) %>% # arranges in alphabetic by location classification, then in descending by test score
   View()
--- R/week13-sql.R
+++ R/week13-sql.R
@@ -1,4 +1,4 @@
-#Script Settings and Resources 
+# Script Settings and Resources 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) 
 library(DBI)
 library(RPostgres)
@@ -7,57 +7,59 @@ library(tidyverse)
 # Data Import and Cleaning
 con <- DBI::dbConnect( # Following instructions for connecting to SQL database in lesson slides
   RPostgres::Postgres(),
-  user = Sys.getenv("NEON_USER"), # maintained a User name and password in the system files on my local harddrive
-  password = Sys.getenv("NEON_PW"), # maintained a User name and password in the system files on my local harddrive
+  user = Sys.getenv("NEON_USER"), # maintained a User name and password in the .Renviron file on my local harddrive usethis::edit_r_environ()
+  password = Sys.getenv("NEON_PW"), # maintained a User name and password in the .Renviron file on my local harddrive usethis::edit_r_environ()
   host = "ep-billowing-union-am14lcnh-pooler.c-5.us-east-1.aws.neon.tech", #host location from instructions
   port = 5432, #port
   dbname = "neondb", #databse name
   sslmode = "require" #SSL requirement
 )
 
-# # Analysis
-# Display the total number of managers; COUNTS employee_id of those that have a test score >= 0, but must ensure joined to the table that has the information on test scores
+# Analysis
+## Display the total number of managers; COUNTS employee_id of those that have a test NOT NULL, but must ensure joined to the table that has the information on test scores
 dbGetQuery(con, "
            SELECT COUNT(employee_id)  
            FROM datascience_employees 
            LEFT JOIN datascience_testscores 
            USING (employee_id)
-           WHERE test_score >= 0 
-           ")
+           WHERE test_score IS NOT NULL
+            AND manager_hire = 'Y'
+           ") # n = 28
 
-# Display the total number of unique managers; COUNTS employee_id of those that have a test score >= 0, but must ensure joined to the table that has the information on test scores, adds DISTINC to make sure only unique numbers are counted
+## Display the total number of unique managers; COUNTS employee_id of those that have a test score NOT NULL, but must ensure joined to the table that has the information on test scores, adds DISTINC to make sure only unique numbers are counted
 dbGetQuery(con, "
            SELECT COUNT(DISTINCT(employee_id))  
            FROM datascience_employees 
            LEFT JOIN datascience_testscores 
            USING (employee_id)
-           WHERE test_score >= 0
-           ")
+           WHERE test_score IS NOT NULL
+            AND manager_hire = 'Y'
+           ") #n = 28
 
 #Display summary of the number of managers split by location (only not original manager hires)
-## SELECT city and counts employee_id WHERE there is a test score >= 0 AND where they were not oriiginally hired as a manager, groups by city and arranges by city ascending (added to match dplyr default)
+## SELECT city and counts employee_id WHERE there is a test score NOT NULL AND where they were not originally hired as a manager, groups by city and arranges by city ascending (added to match dplyr default)
 dbGetQuery(con, "
            SELECT city, COUNT(employee_id) 
            FROM datascience_employees
            LEFT JOIN datascience_testscores
            USING (employee_id)
-           WHERE test_score >= 0 
+           WHERE test_score IS NOT NULL
               AND manager_hire = 'N'
            GROUP BY city
            ORDER BY city ASC
            ")
 
 # Display mean, SD of years of employment, split by performance level
-## SELECTs the grouping variable, performs mean and sd on the yrs_employed, ensuring it grabs from employees that have a test score >= 0, grouping by performance_group
+## SELECTs the grouping variable, performs mean and sd on the yrs_employed, ensuring it grabs from employees that have a test score NOT NULL, grouping by performance_group
 dbGetQuery(con, "
            SELECT 
              performance_group,
-             AVG(yrs_employed),
-             STDDEV(yrs_employed)
+             ROUND(AVG(yrs_employed), 2),
+             ROUND(STDDEV(yrs_employed), 2)
            FROM datascience_employees
            LEFT JOIN datascience_testscores
            USING (employee_id)
-           WHERE test_score >= 0 
+           WHERE test_score IS NOT NULL 
            GROUP BY performance_group
            ")
 
@@ -73,6 +75,7 @@ dbGetQuery(con, "
            USING (employee_id)
            LEFT JOIN datascience_offices o
            ON e.city = o.office
-           WHERE test_score >= 0 
+           WHERE test_score IS NOT NULL
+             AND manager_hire = 'Y'
            ORDER BY office_type ASC, test_score DESC
            ")
